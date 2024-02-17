fc35e7d2a04e6a814e524ca59d2e672b8539efd3
JeanCarlos0111 <130711959+JeanCarlos0111@users.noreply.github.com>
2024-02-17 20:51:37 -0300

    Add files via upload

---
+++ Sistema de Gestão Auxiliar em Condominios/DAL/Conexao.cs
@@ -0,0 +1,36 @@
+﻿using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Sistema_de_Gestão_Auxiliar_em_Condominios.DAL
+{
+    public class Conexao
+    {
+        SqlConnection con = new SqlConnection();
+
+        public Conexao() 
+        {
+            con.ConnectionString = @"Data Source=DESKTOP-DFKVC4V\SQLEXPRESS;Initial Catalog=sgc;Integrated Security=True;";
+        }
+        public SqlConnection conectar()
+        {
+            if(con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            return con;
+        }
+        public void desconectar()
+        {
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+}
---
+++ Sistema de Gestão Auxiliar em Condominios/Modelo/Controle.cs
@@ -0,0 +1,21 @@
+﻿using Sistema_de_Gestão_Auxiliar_em_Condominios.DAL;
+
+namespace Sistema_de_Gestão_Auxiliar_em_Condominios.Modelo
+{
+    public class Controle
+    {
+        public bool tem;
+        public string mensagem = "";
+        public bool acessar(String Login, String Senha) 
+        {
+            LoginDALCommands loginDal = new LoginDALCommands();
+            tem = loginDal.verificarLogin(Login, Senha);
+            if (!loginDal.mensagem.Equals(""))
+            {
+                this.mensagem = loginDal.mensagem;
+            }
+            return tem;
+        
+        }
+    }
+}
